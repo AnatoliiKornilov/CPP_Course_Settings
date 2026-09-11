3b1d9bd1b14f3c6250af2f96b33fe503fffb205d
kornilovanatolii <kornilov.anatolii@phystech.edu>
2026-09-11 21:46:32 +0300

    test std::print

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <print>
+
+int main() {
+    for (int hundred = 1; hundred <= 9; hundred++) {
+        for (int ten  = 0; ten <= 9; ten++) {
+            for (int one = 0; one <= 9; one++) {
+                int number = hundred * 100 + ten * 10 + one;
+                int cube_sum = hundred * hundred * hundred +
+                                        ten * ten * ten +
+                                        one * one * one;
+
+                if (number == cube_sum) {
+                    std::println("{}", number);
+                }
+            }
+        }
+    }
+
+    return 0;
+}
