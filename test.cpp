0d198dfc85110561a673b1ac3fd70687965fffd8
kornilovanatolii <kornilov.anatolii@phystech.edu>
2026-09-11 22:03:35 +0300

    fix code

--- test.cpp
+++ test.cpp
@@ -2,20 +2,19 @@
 #include <print>
 
 int main() {
-    for (int hundred = 1; hundred <= 9; hundred++) {
-        for (int ten  = 0; ten <= 9; ten++) {
-            for (int one = 0; one <= 9; one++) {
-                int number = hundred * 100 + ten * 10 + one;
-                int cube_sum = hundred * hundred * hundred +
-                                        ten * ten * ten +
-                                        one * one * one;
+  for (int hundred = 1; hundred <= 9; hundred++) {
+    for (int ten = 0; ten <= 9; ten++) {
+      for (int one = 0; one <= 9; one++) {
+        int number = hundred * 100 + ten * 10 + one;
+        int cube_sum =
+            hundred * hundred * hundred + ten * ten * ten + one * one * one;
 
-                if (number == cube_sum) {
-                    std::println("{}", number);
-                }
-            }
+        if (number == cube_sum) {
+          std::println("{}", number);
         }
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
